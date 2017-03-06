57309a11f5b2c6bea397c4278547c9ff9751481a
guffy020 <guffy020@gmail.com>
2017-03-06 10:53:45 -0600

    [finished diceduel game]

---
+++ main.py
@@ -0,0 +1,25 @@
+from random import randrange
+print("this is a dice game between player 1 and player 2")
+player1roll=0 
+player2roll=0
+
+winnumber=3
+player1win=0
+player2win=0
+
+
+while(player1win != winnumber and player2win != winnumber):
+    player1roll=randrange(1,6)
+    print(player1roll)
+    player2roll=randrange(1,6)
+    print(player2roll)
+    if(player1roll>player2roll):
+        print("player1 won the round")
+        player1win += 1
+    if(player1roll<player2roll):
+        print("player2 won the round")
+        player2win += 1
+if(player1roll==winnumber):
+    print("player1 won the game")
+if(player2roll==winnumber):
+	print("player2 won the game")
